866de49f707ccc993f0211e8f193395616ec7b2e
Tai Jing <jingtai@uw.edu>
2023-03-13 14:41:50 -0700

    update data and styles

--- js/index.js
+++ js/index.js
@@ -20,12 +20,10 @@ async function addData(id) {
     // Load data from local file
     let response = await fetch(`assets/films/${id}.geojson`);
     let data = await response.json();
-    // map.addSource(`film${data}`, {
-    //     type: 'geojson',
-    //     data: data
-    // });
-    // console.log(data);
-
+    map.addSource(`film${id}`, {
+        type: 'geojson',
+        data: data
+    });
     return data;
 }
 
@@ -40,19 +38,16 @@ function populateList() {
 function createBlocks(data, i) {
     let div = document.createElement("div");
     div.classList.add("film");
-    // div.innerHTML = `<img src="assets/posters/${i}.png" class="poster">
-    // <div class="movie">
-    // <p class="movie_description">${data['features'][0]['properties']['description']}</p>
-    // </div>`;
+
     div.innerHTML = `<img src="assets/posters/${i}.png" class="poster">
-    <p class="movie_description">${data['features'][0]['properties']['description']}</p>`;
-    // div.addEventListener('click', () => {
-    //     layer_curr = `film${i}`
-    //     if (layer_prev != "") map.removeLayer(`film${layer_prev}_points`);
-    //     layer_prev = layer_curr;
-    //     addSource(i);
-    //     addPoints(i);
-    // });
+    <p class="movie_description">${data['properties']['description']}</p>`;
+
+    div.addEventListener('click', () => {
+        layer_curr = `film${i}`
+        if (layer_prev != "") map.removeLayer(`film${layer_prev}_points`);
+        layer_prev = layer_curr;
+        addPoints(i);
+    });
     const container3 = document.getElementById('container3');
     container3.appendChild(div);
 }
@@ -72,18 +67,12 @@ function filterJson (data, keyWord) {
     // console.log(output);
     return output
 }
-function addSource(layer) {
-    map.addSource(`film${layer}_source`, {
-        'type': 'geojson',
-        'data': `film${layer}.geojson`
-    });
-};
 
-function addPoints(layer) {
+function addPoints(id) {
     map.addLayer({
-        'id': `film${layer}_points`,
+        'id': `film${id}_points`,
         'type': 'symbol',
-        'source': `film${layer}_source`,
+        'source': `film${id}`,
         'layout': {
             'icon-image': 'custom-marker',
             'text-field': ['get', 'name'],
